95e84f67cab0c3973e4c3be1ade48f95393e7722
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-07-29 22:16:44 +0530

    Add files via upload

---
+++ SpringCoreDemo2/src/main/java/com/springcoredemo2/CartService.java
@@ -0,0 +1,7 @@
+package com.springcoredemo2;
+
+public class CartService {
+    public void addToCart(){
+        System.out.println("Added to cart");
+    }
+}
---
+++ SpringCoreDemo2/src/main/java/com/springcoredemo2/Main.java
@@ -0,0 +1,7 @@
+package com.springcoredemo2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
